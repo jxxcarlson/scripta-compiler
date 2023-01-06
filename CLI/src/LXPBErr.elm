6cdbfefe68bc2e0dd2361240f165d81c7a53d153
James Carlson <jxxcarlson@gmail.com>
2023-01-06 00:38:59 -0500

    Add CLI/src/LXPBErr.elm

---
+++ CLI/src/LXPBErr.elm
@@ -0,0 +1,31 @@
+module LXPBErr exposing (program)
+
+import Parser.PrimitiveLaTeXBlock exposing (PrimitiveLaTeXBlock, parse_)
+import Posix.IO as IO exposing (IO, Process)
+import Posix.IO.File as File
+import Posix.IO.Process as Proc
+
+
+program : Process -> IO ()
+program process =
+    case process.argv of
+        [ _, filename ] ->
+            IO.do
+                (File.contentsOf filename
+                    |> IO.exitOnError identity
+                )
+            <|
+                \content ->
+                    let
+                        parsed =
+                            content |> String.lines |> parse_
+
+                        blockString =
+                            List.map Parser.PrimitiveLaTeXBlock.printErr parsed.blocks |> String.join "\n\n"
+                    in
+                    IO.do (Proc.print blockString) <|
+                        \_ ->
+                            IO.return ()
+
+        _ ->
+            Proc.logErr "Usage: elm-cli <program> file\n"
