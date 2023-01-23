1fa26a940e8a1cb0b9b13d9e18ad422da20fad2f
James Carlson <jxxcarlson@gmail.com>
2023-01-22 21:31:16 -0500

    Cleanup

--- src/Parser/PrimitiveLaTeXBlock.elm
+++ src/Parser/PrimitiveLaTeXBlock.elm
@@ -168,7 +168,7 @@ nextStep state_ =
                 currentLine =
                     Line.classify (getPosition rawLine state) state.lineNumber rawLine
             in
-            case ClassifyBlock.classify currentLine.content |> Debug.log ("CLASS " ++ (state.lineNumber |> String.fromInt)) of
+            case ClassifyBlock.classify currentLine.content of
                 CBeginBlock label ->
                     Loop (state |> dispatchBeginBlock (CBeginBlock label) currentLine)
 
