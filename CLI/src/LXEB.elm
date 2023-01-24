03ee54cd1fd6283a910318277727928ca0b632dd
James Carlson <jxxcarlson@gmail.com>
2023-01-24 12:13:05 -0500

    Update scripts and add option to CLI/src/LXEB.elm (renamed)

--- CLI/src/LXEB.elm
+++ CLI/src/LXEB.elm
@@ -1,4 +1,4 @@
-module RoundTrip exposing (..)
+module LXEB exposing (..)
 
 import MicroLaTeX.Parser.Pretty
 import Posix.IO as IO exposing (IO, Process)
@@ -6,12 +6,22 @@ import Posix.IO.File as File
 import Posix.IO.Process as Proc
 
 
-analyze : String -> String
-analyze input =
-    let
-        compress str =
-            String.replace "\n" "" str
+analyze : String -> String -> String
+analyze opt input =
+    case opt of
+        "rt" ->
+            analyze1 input
 
+        "p" ->
+            analyze2 input
+
+        _ ->
+            "Unknown option"
+
+
+analyze1 : String -> String
+analyze1 input =
+    let
         output =
             MicroLaTeX.Parser.Pretty.print input
 
@@ -39,20 +49,53 @@ analyze input =
             else
                 "Idempotency match:  NO"
     in
-    [ input, "------", match, inexactMatch, idempotencyMatch, "------", output, "------", output2 ] |> String.join "\n"
+    [ ""
+    , "================"
+    , "Round trip test"
+    , "==============="
+    , input
+    , "------"
+    , match
+    , inexactMatch
+    , idempotencyMatch
+    , "------"
+    , output
+    , "------"
+    , output2
+    , ""
+    ]
+        |> String.join "\n"
+
+
+analyze2 : String -> String
+analyze2 input =
+    let
+        output =
+            MicroLaTeX.Parser.Pretty.print input
+    in
+    [ ""
+    , "=========================="
+    , "Parse to Expression blocks"
+    , "=========================="
+    , input
+    , "------"
+    , output
+    , ""
+    ]
+        |> String.join "\n"
 
 
 program : Process -> IO ()
 program process =
     case process.argv of
-        [ _, filename ] ->
+        [ _, filename, opt ] ->
             IO.do
                 (File.contentsOf filename
                     |> IO.exitOnError identity
                 )
             <|
                 \content ->
-                    IO.do (Proc.print (analyze content)) <|
+                    IO.do (Proc.print (analyze opt content)) <|
                         \_ ->
                             IO.return ()
 
