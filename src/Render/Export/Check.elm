f0a25c63f92c89fed19e209180551abc9929904a
James Carlson <jxxcarlson@gmail.com>
2023-04-18 00:47:53 -0400

    implement and expose: -nonStandardBlockNameList

--- src/Render/Export/Check.elm
+++ src/Render/Export/Check.elm
@@ -1,15 +1,21 @@
-module Render.Export.Check exposing (nonStandardBlocks)
+module Render.Export.Check exposing (nonExportableBlocks, nonExportableExpressions)
 
 import Compiler.ASTTools
 import Parser.Block
 import Render.Block
+import Render.Expression
 import Tree exposing (Tree)
 
 
-nonStandardBlocks : List (Tree.Tree Parser.Block.ExpressionBlock) -> List String
-nonStandardBlocks forest =
-    forest |> Compiler.ASTTools.blockNames |> List.filter (\block -> List.member block nonStandardBlockNameList)
+nonExportableBlocks : List (Tree.Tree Parser.Block.ExpressionBlock) -> List String
+nonExportableBlocks forest =
+    forest |> Compiler.ASTTools.blockNames |> List.filter (\block -> List.member block nonExportableBlockNameList)
 
 
-nonStandardBlockNameList =
-    Render.Block.nonstandardVerbatimBlocks ++ Render.Block.nonStandardOrdinaryBlocks
+nonExportableExpressions : List (Tree.Tree Parser.Block.ExpressionBlock) -> List String
+nonExportableExpressions forest =
+    forest |> Compiler.ASTTools.expressionNames |> List.filter (\expr -> List.member expr Render.Expression.nonstandardElements)
+
+
+nonExportableBlockNameList =
+    Render.Block.nonExportableVerbatimBlocks ++ Render.Block.nonExportableOrdinaryBlocks
